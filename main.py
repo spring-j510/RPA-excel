0c6187be536b62b4681f005e8c265e6353753933
K.I <113268640+spring-j510@users.noreply.github.com>
2023-04-25 07:24:48 +0900

    add data file

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from openpyxl import Workbook
 import function
 
+
+class hr:
+    def __init__(self,name,mail,mobile,adress,motive):
+        self.name = name
+        self.mail = mail
+        self.mobile = mobile
+        self.adress = adress
+        self.motive = motive
+
+
 input_list = []
+
 
 for i in range(5):
     input_list.append(str(input()))
+
+print(input_list)
+
 """
 name:Mark
 mail:asdfghjk12345@spring-j510.com
@@ -13,16 +27,20 @@ adress:France paris
 reason for (one's) desire:As an AI language model
 """
 
-class hr:
-    def __init__(self,name,mail,mobile,motive):
-        self.name = name[:5]
-        self.mail = mail
-        self.mobile = mobile
-        self.motive = motive
 
-
-myinstance = hr(input_list)
+myinstance = hr(input_list[0],input_list[1],input_list[2],input_list[3],input_list[4])
 print(myinstance.name)
+
+
+# candidate = []
+
+# candidates.append(1)
+
+# print(candidates)
+
+# candidate = hr('サンドイッチ', 500)
+
+
 
 """
 
@@ -35,41 +53,42 @@ adress:
 reason for (one's) desire:
 """
 
+path = "xlsx/" + function.excel_title()
 
-# # ワークブックの新規作成と保存
-# wb = Workbook()
-# wb.save(function.excel_title())
+# ワークブックの新規作成と保存
+wb = Workbook()
+wb.save(path)
 
-# # ワークブックの読み込み
-# from openpyxl import load_workbook
+# ワークブックの読み込み
+from openpyxl import load_workbook
 
-# wb = load_workbook(function.excel_title())
+wb = load_workbook(path)
 
-# # 変更したいシート名を指定する
-# old_sheet_name = 'Sheet'
-# new_sheet_name = 'summary'
+# 変更したいシート名を指定する
+old_sheet_name = 'Sheet'
+new_sheet_name = 'summary'
 
-# # シート名を変更する
-# ws = wb[old_sheet_name]
-# ws.title = new_sheet_name
-
-
-# # ワークシートの選択
-# ws = wb['summary']  # ワークシートを指定
-# ws = wb.active  # アクティブなワークシートを選択
-
-# # セルに書き込み
-# ws['B2'] = 'No.'
-# ws['C2'] = 'name'
-# ws['D2'] = 'Mail'
-# ws['E2'] = 'Mobile'
-# ws['F2'] = 'Adress'
-# ws['G2'] = 'reason for (ones) desire'
+# シート名を変更する
+ws = wb[old_sheet_name]
+ws.title = new_sheet_name
 
 
+# ワークシートの選択
+ws = wb['summary']  # ワークシートを指定
+ws = wb.active  # アクティブなワークシートを選択
 
+# セルに書き込み
+ws['B2'] = 'No.'
+ws['C2'] = 'name'
+ws['D2'] = 'Mail'
+ws['E2'] = 'Mobile'
+ws['F2'] = 'Adress'
+ws['G2'] = 'reason for (ones) desire'
 
 
 
 
-# wb.save(function.excel_title())  # overwrite myworkbook.xlsx
+
+
+
+wb.save(path)  # overwrite myworkbook.xlsx
